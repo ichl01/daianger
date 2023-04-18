8caa635581eeb1b31a9abe20f7d8119a4355c399
ichl01 <koukou000001@outlook.com>
2023-04-18 18:50:59 +0900

    デザインの変更とバグ修正と内容の追加

--- db.js
+++ db.js
@@ -1,17 +1,93 @@
-var obj = [{
-    subject: "国語",
-    content: "プリント2枚",
-    due: new Date(2023, 4, 17, 0, 0)
-}, {
-    subject: "数学",
-    content: "テスト直し & 発展課題",
-    due: new Date(2023, 4, 17, 0, 0)
-}, {
-    subject: "英語",
-    content: "Breakthrough Workbook P.24〜P.35",
-    due: new Date(2023, 4, 17, 0, 0)
-}, {
+//データベース
+var obj = [
+  {
     subject: "英語",
     content: "テスト直し",
-    due: new Date(2023, 4, 21, 0, 0)
-}];
+    due: new Date(2023, 3, 21, 0, 0)
+  }, {
+    subject: "国語",
+    content: "プリント2枚",
+    due: new Date(2023, 3, 17, 0, 0)
+  }, {
+    subject: "数学",
+    content: "テスト直し & 発展課題",
+    due: new Date(2023, 3, 17, 0, 0)
+  }, {
+    subject: "英語",
+    content: "Breakthrough Workbook P.24〜P.35",
+    due: new Date(2023, 3, 17, 0, 0)
+  }, {
+    subject: "現代文",
+    content: "プリント: 2(随)山内美郷",
+    due: new Date(2023, 3, 24, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 1御伽草子",
+    due: new Date(2023, 3, 24, 0, 0)
+  }, {
+    subject: "漢文",
+    content: "プリント: 1「説苑」",
+    due: new Date(2023, 4, 1, 0, 0)
+  }, {
+    subject: "現代文",
+    content: "プリント: 3(評)平野啓一郎, 4(小)川端康成",
+    due: new Date(2023, 4, 8, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 2古今著聞集, 3徒然草",
+    due: new Date(2023, 4, 8, 0, 0)
+  }, {
+    subject: "漢文",
+    content: "プリント: 3「十八史略」~5「論語」まで",
+    due: new Date(2023, 4, 8, 0, 0)
+  }, {
+    subject: "現代文",
+    content: "プリント: 5(評)鈴木孝夫",
+    due: new Date(2023, 4, 22, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 4十訓抄",
+    due: new Date(2023, 4, 22, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 5花月草子",
+    due: new Date(2023, 4, 29, 0, 0)
+  }, {
+    subject: "漢文",
+    content: "プリント: 6「後漢書」",
+    due: new Date(2023, 4, 29, 0, 0)
+  }, {
+    subject: "現代文",
+    content: "プリント: 6(評)加藤秀俊",
+    due: new Date(2023, 5, 5, 0, 0)
+  }, {
+    subject: "漢文",
+    content: "プリント: 7「閲微草堂筆記」",
+    due: new Date(2023, 5, 5, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 6伊曽保物語",
+    due: new Date(2023, 5, 12, 0, 0)
+  }, {
+    subject: "現代文",
+    content: "プリント: 7(小)重松清",
+    due: new Date(2023, 5, 19, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 7古今著聞集",
+    due: new Date(2023, 5, 19, 0, 0)
+  }, {
+    subject: "漢文",
+    content: "プリント: 8「戦国策」",
+    due: new Date(2023, 5, 26, 0, 0)
+  }];
+//データベースの並び替え(日付順)
+obj.sort(function (a, b) {
+  if (a.due > b.due) {
+    return 1;
+  } else {
+    return -1;
+  }
+})
+console.log(obj)
+console.log(obj[0].due)
--- homework.js
+++ homework.js
@@ -1,23 +1,29 @@
-
-
+//現在の日付を取得
 var today = new Date()
 var nowdate = today.getDate();
-var nowmonth = today.getMonth() + 1;
 
 function homework() {
 
     for (var i = 0; i < obj.length; i++) {
+
+        //課題締切の月日を定義
         let datedue = obj[i].due.getDate();
-        let monthdue = obj[i].due.getMonth();
-        console.log("datedue: " + datedue + " monthdue: " + monthdue)
+        let monthdue = obj[i].due.getMonth() +1;
+        console.log("due: " + monthdue + "/" + datedue)
+
+        //要素を挿入
         let content = '<div class="homework"><table><tr><td>教科:</td><td>' + obj[i].subject + '</td></tr><tr><td>範囲:</td><td>' + obj[i].content + '</td></tr><tr><td>提出日:</td><td>' + monthdue + "月" + datedue + "日" + '</td></tr></table></div>'
-        if ((datedue - nowdate) / (1000 * 60 * 60 * 24) <= 7 && datedue > nowdate) {
+        console.log(obj[i].due - today)
+        if ((obj[i].due - today) / (1000 * 60 * 60 * 24) <= 7 && obj[i].due - today > 0) {
             document.getElementById('upcoming').insertAdjacentHTML('beforeend', content);
-        } else if (datedue < nowdate) {
+        } else if (obj[i].due - today < 0) {
             document.getElementById('past').insertAdjacentHTML('beforeend', content);
         } else {
             document.getElementById('others').insertAdjacentHTML('beforeend', content);
         }
+        //1つ目のif:7日以内の課題を振り分け
+        //2つ目のif:過去の課題を振り分け
+        //else それ以外の場合
     }
 
 }
