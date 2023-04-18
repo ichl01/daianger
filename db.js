d5410c3c14f1ace9052776eb5800c8d88ec1d7c4
ichl01 <koukou000001@outlook.com>
2023-04-18 21:10:51 +0900

    内容の追加

--- db.js
+++ db.js
@@ -80,6 +80,90 @@ var obj = [
     subject: "漢文",
     content: "プリント: 8「戦国策」",
     due: new Date(2023, 5, 26, 0, 0)
+  }, {
+    subject: "現代文",
+    content: "プリント: 8(評)日高敏隆",
+    due: new Date(2023, 6, 10, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 8北辺随筆",
+    due: new Date(2023, 6, 10, 0, 0)
+  }, {
+    subject: "古文",
+    content: "プリント: 9おらが春",
+    due: new Date(2023, 6, 18, 0, 0)
+  },{
+    subject: "漢文",
+    content: "プリント: 9「三国志」",
+    due: new Date(2023, 6, 18, 0, 0)
+  },{
+    subject: "現代文",
+    content: "プリント: 9(評)山崎正和",
+    due: new Date(2023, 6, 24, 0, 0)
+  },{
+    subject: "漢文",
+    content: "プリント: 10 唐詩選",
+    due: new Date(2023, 6, 24, 0, 0)
+  },{
+    subject: "現代文",
+    content: "プリント: 10(小)佐藤多佳子~14(評)外山滋比古まで",
+    due: new Date(2023, 7, 17, 0, 0)
+  },{
+    subject: "古文",
+    content: "プリント: 10今昔物語集, 14今昔物語集",
+    due: new Date(2023, 7, 17, 0, 0)
+  },{
+    subject: "漢文",
+    content: "プリント: 新傾向問題1~13「史記」",
+    due: new Date(2023, 7, 17, 0, 0)
+  },{
+    subject: "現代文",
+    content: "プリント: 15(評)柏木博",
+    due: new Date(2023, 7, 28, 0, 0)
+  },{
+    subject: "古文",
+    content: "プリント: 15徒然草, 16十訓抄",
+    due: new Date(2023, 7, 28, 0, 0)
+  },{
+    subject: "漢文",
+    content: "プリント: 14 唐詩選, 15「太尉文正王...」",
+    due: new Date(2023, 7, 28, 0, 0)
+  },{
+    subject: "数学A",
+    content: "授業プリント2枚(先週の)",
+    due: new Date(2023, 3, 20, 0, 0)
+  },{
+    subject: "英語",
+    content: "OVER ALL 2: UNIT 1・2",
+    due: new Date(2023, 3, 24, 0, 0)
+  },{
+    subject: "英語",
+    content: "Breakthrough ワークブック p.44~55,p.64~69",
+    due: new Date(2023, 4, 8, 0, 0)
+  },{
+    subject: "英語",
+    content: "OVER ALL 2: UNIT 3~7",
+    due: new Date(2023, 4, 8, 0, 0)
+  },{
+    subject: "英語",
+    content: "Breakthrough ワークブック p.76~p.89",
+    due: new Date(2023, 4, 22, 0, 0)
+  },{
+    subject: "英語",
+    content: "OVER ALL 2: UNIT 8・9",
+    due: new Date(2023, 4, 29, 0, 0)
+  },{
+    subject: "英語",
+    content: "OVER ALL 2: UNIT 10・11",
+    due: new Date(2023, 5, 5, 0, 0)
+  },{
+    subject: "英語",
+    content: "OVER ALL 2: UNIT 12・13",
+    due: new Date(2023, 5, 12, 0, 0)
+  },{
+    subject: "英語",
+    content: "OVER ALL 2: UNIT 14・15",
+    due: new Date(2023, 5, 19, 0, 0)
   }];
 //データベースの並び替え(日付順)
 obj.sort(function (a, b) {
