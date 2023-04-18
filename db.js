c1e32c01fa6ad793f89602e43e1d0939098eed82
ichl01 <koukou000001@outlook.com>
2023-04-18 16:49:41 +0900

    re-solved

---
+++ db.js
@@ -0,0 +1,17 @@
+var obj = [{
+    subject: "国語",
+    content: "プリント2枚",
+    due: new Date(2023, 4, 17, 0, 0)
+}, {
+    subject: "数学",
+    content: "テスト直し & 発展課題",
+    due: new Date(2023, 4, 17, 0, 0)
+}, {
+    subject: "英語",
+    content: "Breakthrough Workbook P.24〜P.35",
+    due: new Date(2023, 4, 17, 0, 0)
+}, {
+    subject: "英語",
+    content: "テスト直し",
+    due: new Date(2023, 4, 21, 0, 0)
+}];
