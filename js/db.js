cce5562be86f7fbdbe817524dde11c74e75ec95f
ichl01 <koukou000001@outlook.com>
2023-06-09 20:17:22 +0900

    課題追加&古文暫定メッセージ表示

--- js/db.js
+++ js/db.js
@@ -340,6 +340,11 @@ var obj = [
     content: "ALL EARS STANDARD",
     due: new Date(2023, 5, 26, 0, 0),
     id: 109
+  }, {
+    subject: "古文",
+    content: "テスト直し",
+    due: new Date(2023, 5, 12, 0, 0),
+    id: 109
   }];
 //データベースの並び替え(日付順)
 obj.sort(function (a, b) {
