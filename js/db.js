6628245ac722723ea168cb66b2138104b3cceac6
ichl01 <koukou000001@outlook.com>
2023-07-21 17:05:19 +0900

    夏休みの情報の追加

--- js/db.js
+++ js/db.js
@@ -1,55 +1,10 @@
 //データベース
 var obj = [
   {
-    subject: "現代文",
-    content: "プリント: 5(評)鈴木孝夫",
-    due: new Date(2023, 4, 22, 0, 0),
-    id: 10
-  }, {
-    subject: "古文",
-    content: "テキスト: 4十訓抄",
-    due: new Date(2023, 4, 22, 0, 0),
-    id: 11
-  }, {
-    subject: "古文",
-    content: "テキスト: 5花月草子",
-    due: new Date(2023, 4, 29, 0, 0),
-    id: 12
-  }, {
-    subject: "漢文",
-    content: "プリント: 6「後漢書」",
-    due: new Date(2023, 4, 29, 0, 0),
-    id: 13
-  }, {
-    subject: "現代文",
-    content: "プリント: 6(評)加藤秀俊",
-    due: new Date(2023, 5, 5, 0, 0),
-    id: 14
-  }, {
-    subject: "漢文",
-    content: "プリント: 7「閲微草堂筆記」",
-    due: new Date(2023, 5, 5, 0, 0),
-    id: 15
-  }, {
-    subject: "古文",
-    content: "テキスト: 6伊曽保物語",
-    due: new Date(2023, 5, 19, 0, 0),
-    id: 16
-  }, {
-    subject: "現代文",
-    content: "プリント: 7(小)重松清",
-    due: new Date(2023, 5, 19, 0, 0),
-    id: 17
-  }, {
     subject: "古文",
     content: "テキスト: 7古今著聞集",
     due: new Date(2023, 6, 10, 0, 0),
     id: 18
-  }, {
-    subject: "漢文",
-    content: "プリント: 8「戦国策」",
-    due: new Date(2023, 5, 26, 0, 0),
-    id: 19
   }, {
     subject: "現代文",
     content: "プリント: 8(評)日高敏隆",
@@ -82,7 +37,7 @@ var obj = [
     id: 26
   }, {
     subject: "古文",
-    content: "テキスト: 9おらが春~13宇治拾遺物語",
+    content: "テキスト: 9おらが春~14宇治拾遺物語",
     due: new Date(2023, 7, 17, 0, 0),
     id: 27
   }, {
@@ -97,7 +52,7 @@ var obj = [
     id: 29
   }, {
     subject: "古文",
-    content: "テキスト: 14今昔物語集,15徒然草",
+    content: "テキスト: 15徒然草",
     due: new Date(2023, 7, 28, 0, 0),
     id: 30
   }, {
@@ -105,121 +60,6 @@ var obj = [
     content: "プリント: 14 唐詩選, 15「太尉文正王...」",
     due: new Date(2023, 7, 28, 0, 0),
     id: 31
-  }, {
-    subject: "英語",
-    content: "Breakthrough ワークブック p.76~p.89",
-    due: new Date(2023, 4, 22, 0, 0),
-    id: 36
-  }, {
-    subject: "英語",
-    content: "OVER ALL 2: UNIT 8・9",
-    due: new Date(2023, 4, 29, 0, 0),
-    id: 37
-  }, {
-    subject: "英語",
-    content: "OVER ALL 2: UNIT 10・11",
-    due: new Date(2023, 5, 5, 0, 0),
-    id: 38
-  }, {
-    subject: "英語",
-    content: "OVER ALL 2: UNIT 12・13",
-    due: new Date(2023, 5, 12, 0, 0),
-    id: 39
-  }, {
-    subject: "英語",
-    content: "OVER ALL 2: UNIT 14・15",
-    due: new Date(2023, 5, 19, 0, 0),
-    id: 40
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第82回",
-    due: new Date(2023, 4, 24, 0, 0),
-    id: 50
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第84回",
-    due: new Date(2023, 4, 25, 0, 0),
-    id: 51
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 4, 26, 0, 0),
-    id: 52
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 4, 30, 0, 0),
-    id: 53
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第86回",
-    due: new Date(2023, 4, 31, 0, 0),
-    id: 54
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第87回",
-    due: new Date(2023, 5, 1, 0, 0),
-    id: 55
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 2, 0, 0),
-    id: 56
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 6, 0, 0),
-    id: 57
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第89回",
-    due: new Date(2023, 5, 8, 0, 0),
-    id: 58
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 9, 0, 0),
-    id: 59
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 13, 0, 0),
-    id: 60
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第91回",
-    due: new Date(2023, 5, 14, 0, 0),
-    id: 61
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第88回",
-    due: new Date(2023, 5, 15, 0, 0),
-    id: 62
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 16, 0, 0),
-    id: 63
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 20, 0, 0),
-    id: 64
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第90回",
-    due: new Date(2023, 5, 21, 0, 0),
-    id: 65
-  }, {
-    subject: "数学Ⅰ",
-    content: "今日ドリ 第92回",
-    due: new Date(2023, 5, 22, 0, 0),
-    id: 66
-  }, {
-    subject: "数学",
-    content: "挑戦状",
-    due: new Date(2023, 5, 23, 0, 0),
-    id: 67
   }, {
     subject: "数学",
     content: "挑戦状",
@@ -275,77 +115,12 @@ var obj = [
     content: "今日ドリ 第7回",
     due: new Date(2023, 6, 27, 0, 0),
     id: 78
-  }, {
-    subject: "英語",
-    content: "ALL EARS STANDARD: Lesson1~10",
-    due: new Date(2023, 4, 29, 0, 0),
-    id: 87
-  }, {
+  },{
     subject: "お知らせ",
-    content: "中間考査以前の課題は削除いしました。お問い合わせいただければお探ししますのでお求めの方はサイト管理者までお問い合わせください。",
-    due: new Date(2023, 4, 19, 0, 0),
+    content: "期末考査以前の課題は削除いしました。お問い合わせいただければお探ししますのでお求めの方はサイト管理者までお問い合わせください。",
+    due: new Date(2023, 6, 17, 0, 0),
     id: 99
-  }, {
-    subject: "数学",
-    content: "進研模試過去問2021に挑戦",
-    due: new Date(2023, 4, 22, 0, 0),
-    id: 100
-  }, {
-    subject: "数学Ⅰ+A",
-    content: "中間考査直し",
-    due: new Date(2023, 4, 29, 0, 0),
-    id: 101
-  }, {
-    subject: "数学",
-    content: "進研模試過去問2020に挑戦",
-    due: new Date(2023, 5, 5, 0, 0),
-    id: 102
-  }, {
-    subject: "数学",
-    content: "実力考査直し",
-    due: new Date(2023, 5, 12, 0, 0),
-    id: 103
-  }, {
-    subject: "数学",
-    content: "進研模試過去問2019に挑戦",
-    due: new Date(2023, 5, 19, 0, 0),
-    id: 104
-  }, {
-    subject: "数学",
-    content: "(数学Ⅰ+A一学期中間考査合計150点以上の人と希望者) 駿台ハイレベル模試(一部)をノートに問題を貼って解いて提出",
-    due: new Date(2023, 4, 29, 0, 0),
-    id: 105
-  }, {
-    subject: "英語A",
-    content: "テスト直し",
-    due: new Date(2023, 5, 2, 0, 0),
-    id: 106
-  }, {
-    subject: "論理・表現",
-    content: "テスト直し",
-    due: new Date(2023, 5, 2, 0, 0),
-    id: 107
-  }, {
-    subject: "化学基礎",
-    content: "テスト直し",
-    due: new Date(2023, 5, 6, 0, 0),
-    id: 108
-  }, {
-    subject: "英語",
-    content: "ALL EARS STANDARD",
-    due: new Date(2023, 5, 26, 0, 0),
-    id: 109
-  }, {
-    subject: "古文",
-    content: "テスト直し",
-    due: new Date(2023, 5, 12, 0, 0),
-    id: 109
-  }, {
-    subject: "英語",
-    content: "WIDE ANGLE Book 3: Lesson 1,2",
-    due: new Date(2023, 5, 26, 0, 0),
-    id: 110
-  }, {
+  },  {
     subject: "英語",
     content: "WIDE ANGLE Book 3: Lesson 3,4",
     due: new Date(2023, 6, 10, 0, 0),
@@ -380,12 +155,7 @@ var obj = [
     content: "授業ノート",
     due: new Date(2023, 6, 4, 0, 0),
     id: 118
-  }, {
-    subject: "公共β",
-    content: "サブノート P.10~P.13",
-    due: new Date(2023, 5, 29, 0, 0),
-    id: 119
-  }, {
+  },  {
     subject: "公共α",
     content: "スタディノート P.42~P.57",
     due: new Date(2023, 6, 4, 0, 0),
@@ -430,11 +200,6 @@ var obj = [
     content: "Advanced Exercise / Workbook Lesson3~5",
     due: new Date(2023, 6, 6, 0, 0),
     id: 128
-  }, {
-    subject: "英語B",
-    content: "Breakthrough英文法27章 Optional 10 仮定法 L27 話法 (P.76~P.79) (授業で行う)",
-    due: new Date(2023, 5, 28, 0, 0),
-    id: 129
   }, {
     subject: "化学基礎",
     content: "徹底ドリル p.22~p.25,p.26の3",
@@ -493,7 +258,7 @@ var obj = [
   }, {
     subject: "英語",
     content: "テスト直し",
-    due: new Date(2023, 6, 17, 0, 0),
+    due: new Date(2023, 6, 18, 0, 0),
     id: 141
   }, {
     subject: "公共Β",
@@ -520,6 +285,76 @@ var obj = [
     content: "読書感想文",
     due: new Date(2023, 7, 17, 0, 0),
     id: 144
+  }, {
+    subject: "化学基礎",
+    content: "テスト直し",
+    due: new Date(2023, 6, 18, 0, 0),
+    id: 145
+  }, {
+    subject: "英語",
+    content: "英語A Advanced Exercise / Workbook: Reading1とLesson6",
+    due: new Date(2023, 7, 17, 0, 0),
+    id: 146
+  }, {
+    subject: "英語",
+    content: "英語B NT WORKBOOK: Lesson4 ~ Lesson5",
+    due: new Date(2023, 7, 17, 0, 0),
+    id: 147
+  }, {
+    subject: "英語",
+    content: "論理・表現 Earthrise ワークブック: Lesson10",
+    due: new Date(2023, 7, 17, 0, 0),
+    id: 148
+  }, {
+    subject: "英語",
+    content: "岡山県高校生レターコンテスト",
+    due: new Date(2023, 7, 17, 0, 0),
+    id: 149
+  }, {
+    subject: "英語",
+    content: "NEWSBREAKERS(総合設問集を提出)",
+    due: new Date(2023, 7, 28, 0, 0),
+    id: 150
+  }, {
+    subject: "英語",
+    content: "CNN Workbook",
+    due: new Date(2023, 7, 28, 0, 0),
+    id: 151
+  }, {
+    subject: "公共",
+    content: "スタディーノート: P.58~P.71",
+    due: new Date(2023, 7, 17, 0, 0),
+    id: 152
+  }, {
+    subject: "公共",
+    content: "日本のマスメディアについて知ろう!(プリント)",
+    due: new Date(2023, 7, 28, 0, 0),
+    id: 153
+  }, {
+    subject: "地理総合",
+    content: "白地図プリント",
+    due: new Date(2023, 7, 25, 0, 0),
+    id: 154
+  }, {
+    subject: "地理総合",
+    content: "作文(Google Classroom参照)",
+    due: new Date(2023, 7, 25, 0, 0),
+    id: 155
+  }, {
+    subject: "化学基礎",
+    content: "補習で配布するTSP",
+    due: new Date(2023, 7, 28, 0, 0),
+    id: 156
+  }, {
+    subject: "生物基礎",
+    content: "プログレス生物基礎: P.38~P.39",
+    due: new Date(2023, 7, 28, 0, 0),
+    id: 157
+  }, {
+    subject: "進路",
+    content: "オープンキャンパス参加シート",
+    due: new Date(2023, 7, 28, 0, 0),
+    id: 158
   }];
 //データベースの並び替え(日付順)
 obj.sort(function (a, b) {
