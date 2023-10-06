31e4507262c0c40b553c64afb8fb5cf368c8fd8a
ichl01 <koukou000001@outlook.com>
2023-10-06 23:19:39 +0900

    mainにすべての要素を格納

--- js/move.js
+++ js/move.js
@@ -1,3 +1,10 @@
 function go(place) {
     location.href = place;
 }
+
+function load(place){
+    let elem = document.getElementById('main')
+    if(place == index){
+        document.title = "トップページ - Daianger";
+    }
+}
